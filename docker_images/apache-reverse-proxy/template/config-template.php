12fea053dc67d13dd309c0d95f4cf2dd48a23311
maelle <maelle.vogel@heig-vd.ch>
2022-01-16 18:08:50 +0100

    correct path pets

--- docker_images/apache-reverse-proxy/template/config-template.php
+++ docker_images/apache-reverse-proxy/template/config-template.php
@@ -6,8 +6,8 @@ $DYNAMIC_APP = getenv('DYNAMIC_APP');
 <VirtualHost *:80>
         ServerName demo.api.ch
 
-        ProxyPass '/api/students/' 'http://<?php print "$DYNAMIC_APP"?>/'
-        ProxyPassReverse '/api/students/' 'http://<?php print "$DYNAMIC_APP"?>/' 
+        ProxyPass '/pets/' 'http://<?php print "$DYNAMIC_APP"?>/'
+        ProxyPassReverse '/pets/' 'http://<?php print "$DYNAMIC_APP"?>/' 
 
         ProxyPass '/' 'http://<?php print "$STATIC_APP"?>/'
         ProxyPassReverse '/' 'http://<?php print "$STATIC_APP"?>/'
